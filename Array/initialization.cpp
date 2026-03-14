720c89ea550cab1ad5f847424dbf61be630064ff
adarsh kushwaha <ak6862219@gmail.com>
2026-03-14 17:40:53 +0000

    update

--- Array/initialization.cpp
+++ Array/initialization.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main(){
     //there are many type of initializaton of array
-    int arr[100];//in this array 100 columns and all same size and countigous locations
-    int arr[5]={2,4,6,8,10};
+    int arr1[100];//in this array 100 columns and all same size and countigous locations
+    int arr2[5]={2,4,6,8,10};
     //     index 0 1 2 3 4   index = n-1 
-    int arr[] = {2,4,6,7,8} //isme array ke size to diya but arr khud se create kar lega
+    int arr3[] = {2,4,6,7,8}; //isme array ke size to diya but arr khud se create kar lega
          
 }
 
